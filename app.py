68fba7cfa4af16e7fca58ae1eaff722fb0bb9638
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2026-01-01 14:48:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def main():
             st.info("Escolha um módulo no painel à esquerda.")
 
         elif st.session_state.active_module == "inertial_rec":
-            inertial_file.render()  # renderiza o módulo DENTRO da interface principal
+            inertial_file.render()
 
 if __name__ == "__main__":
     main()
