d35daaf86377023ff03e1423a0fb9cd6a76cc636
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2026-01-01 14:43:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def main():
     with col1:
         st.image("b1.png", width=220)
         if st.button("Sensor Inercial (Norma)", use_container_width=True):
-            st.session_state.active_module = "gyro"
+            st.session_state.active_module = "inertial_rec"
 
         if st.button("Home", use_container_width=True):
             st.session_state.active_module = "home"
@@ -24,7 +24,7 @@ def main():
         if st.session_state.active_module == "home":
             st.info("Escolha um módulo no painel à esquerda.")
 
-        elif st.session_state.active_module == "gyro":
+        elif st.session_state.active_module == "inertial_rec":
             gyro_module.render()  # renderiza o módulo DENTRO da interface principal
 
 if __name__ == "__main__":
