b9c25de09ccb17278179e91e778a47b4d2b24ade
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2026-01-01 19:10:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import inertial_file
 
 def main():
-    st.set_page_config(page_title="Momentum Web", layout="wide")
+    st.set_page_config(page_title="Momentum Web app", layout="wide")
 
     st.markdown(
         """
